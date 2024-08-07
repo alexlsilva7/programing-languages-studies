6c58a24f29f9411408fd181dbae11420b8206d9d
Alex Lopes <alex.lsilva154@gmail.com>
2024-08-07 09:39:38 -0300

    ponteiros

--- go/ponteiros/main.go
+++ go/ponteiros/main.go
@@ -10,4 +10,18 @@ func main() {
 	println(a, ponteiro, *ponteiro) // 20 0xc000065f38 20
 	b := &a                         // b é um ponteiro para a
 	println(b, *b)                  // 0xc000065f38 20
+
+	println(soma(10, 20)) // 30
+
+	var result int = 0
+	somaResult(1, 2, &result)
+	println(result)
+}
+
+func soma(a, b int) int { // passagem de uma cópia do valor
+	return a + b
+}
+
+func somaResult(a int, b int, result *int) { // passagem de um ponteiro para o valor
+	*result = a + b
 }
